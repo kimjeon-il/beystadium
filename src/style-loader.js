2ac9b598bd84664b3d4c14c1dc604c8c74bc191f
kimjeon-il <55841736+kimjeon-il@users.noreply.github.com>
2026-07-31 18:44:04 +0900

    feat(mobile): update src/style-loader.js

--- src/style-loader.js
+++ src/style-loader.js
@@ -1,4 +1,4 @@
-const styleVersion = "20260726-composition-eight-rows";
+const styleVersion = "20260731-mobile-overhaul";
 const styleOrder = ["page", "collection", "table", "release", "anime", "catalog", "search", "modal"];
 const styleFiles = Object.fromEntries(styleOrder.map(key => [key, `./styles/${key}.css?v=${styleVersion}`]));
 const stylePromises = new Map();
